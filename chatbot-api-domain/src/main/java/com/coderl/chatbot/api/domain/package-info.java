1818830c15c4447135b655857c04a53a4c28b838
86158 <2096099603@qq.com>
2024-06-18 22:14:42 +0800

    coderL feat:init project

---
+++ chatbot-api-domain/src/main/java/com/coderl/chatbot/api/domain/package-info.java
@@ -0,0 +1,1 @@
+package com.coderl.chatbot.api.domain;
---
+++ chatbot-api-interfaces/src/test/java/com/coderl/chatbot/api/test/ApiTest.java
@@ -0,0 +1,10 @@
+package com.coderl.chatbot.api.test;
+
+/**
+ * 单元测试
+ */
+public class ApiTest {
+    public static void main(String[] args) {
+
+    }
+}
